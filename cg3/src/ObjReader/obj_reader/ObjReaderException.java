355543b5130b52e92b4d9e0550aafd435fb4a041
tdoel-gg <dinakardko18@gmail.com>
2024-12-15 22:25:36 +0300

    Delete cg3 directory

--- cg3/src/ObjReader/obj_reader/ObjReaderException.java
+++
@@ -1,7 +0,0 @@
-package ObjReader.obj_reader;
-
-public class ObjReaderException extends RuntimeException {
-    public ObjReaderException(String errorMessage, int lineInd) {
-        super("Error parsing OBJ file on line: " + lineInd + ". " + errorMessage);
-    }
-}
--- cg3/src/cs/vsu/ru/k2/g42/myshkevich_a_n/objreader/ObjReaderException.java
+++
@@ -1,7 +0,0 @@
-package cs.vsu.ru.k2.g42.myshkevich_a_n.objreader;
-
-public class ObjReaderException extends RuntimeException {
-    public ObjReaderException(String errorMessage, int lineInd) {
-        super("Error parsing OBJ file on line: " + lineInd + ". " + errorMessage);
-    }
-}
